1ec5e69603083b492cefbe5770ce8793af8d3c56
Jano at Zelda <alejandro@mosteo.com>
2018-02-08 16:19:40 +0100

    Added per-level log procedures

--- src/simple_logging.adb
+++ src/simple_logging.adb
@@ -22,4 +22,58 @@ package body Simple_Logging is
       end if;
    end Log;
 
+   ------------
+   -- Always --
+   ------------
+
+   procedure Always  (Msg : String) is
+   begin
+      Log (Msg, Always);
+   end Always;
+
+   -----------
+   -- Error --
+   -----------
+
+   procedure Error   (Msg : String) is
+   begin
+      Log (Msg, Error);
+   end Error;
+
+   -------------
+   -- Warning --
+   -------------
+
+   procedure Warning (Msg : String) is
+   begin
+      Log (Msg, Warning);
+   end Warning;
+
+   ----------
+   -- Info --
+   ----------
+
+   procedure Info    (Msg : String) is
+   begin
+      Log (Msg, Info);
+   end Info;
+
+   ------------
+   -- Detail --
+   ------------
+
+   procedure Detail  (Msg : String) is
+   begin
+      Log (Msg, Detail);
+   end Detail;
+
+   -----------
+   -- Debug --
+   -----------
+
+   procedure Debug   (Msg : String) is
+   begin
+      Log (Msg, Debug);
+   end Debug;
+
 end Simple_Logging;
--- src/simple_logging.ads
+++ src/simple_logging.ads
@@ -14,4 +14,14 @@ package Simple_Logging with Preelaborate is
    procedure Log (S : String; Level : Levels := Info);
    --  Report a log message
 
+   --  Log by level.
+   --  Useful if you want to use a package renaming as prefix, e.g.: Log.Info ("Blah");
+
+   procedure Always  (Msg : String);
+   procedure Error   (Msg : String);
+   procedure Warning (Msg : String);
+   procedure Info    (Msg : String);
+   procedure Detail  (Msg : String);
+   procedure Debug   (Msg : String);
+
 end Simple_Logging;
